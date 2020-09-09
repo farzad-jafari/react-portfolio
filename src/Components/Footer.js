b538481ca32ca5cba773869ade45b42b2a0b54fb
Farzad <f.jafari8990@gmail.com>
2020-09-09 12:29:01 +0430

    customize

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,11 +19,11 @@ class Footer extends Component {
            </ul>
 
            <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
+            <li>♥️</li>
            </ul>
 
            <ul className="copyright">
-              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
+              <li>&copy;  - Copyright 2020</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
